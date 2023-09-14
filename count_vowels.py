104a1e6c30ae027db4c6085a43f9d30806f77a13
amina-sm <aminamayengo18@gmail.com>
2023-09-14 13:33:21 +0300

    Count vowel challenge

---
+++ count_vowels.py
@@ -0,0 +1,13 @@
+def fun(word):
+    lis=[]
+    vow=['a','e','i','o','u']
+    for c in word:
+        for v in vow:
+                    if c==v:
+                            if  c not in lis:
+                                lis.append(c)
+    print("Total unique vowels: ",len(lis))
+    return lis
+name="amina" 
+print(fun(name))
+                    
