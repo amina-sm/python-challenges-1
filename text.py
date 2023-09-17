0ebb12ad96cb76deb23e7847485bd332aecd72f8
amina-sm <aminamayengo18@gmail.com>
2023-09-17 14:07:40 +0300

    text

---
+++ text.py
@@ -0,0 +1,17 @@
+# def fun():
+#     return print(5)
+
+# fun()
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:4])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if "mango" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+    
+    
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
